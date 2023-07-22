3e6749b7dca5e97d1d2eb12f930e81f42ff08bd1
radiantly <buildingsomethingfun@gmail.com>
2023-07-22 12:48:49 +0000

    Create nginx conf

--- main.go
+++ main.go
@@ -29,15 +29,64 @@ func CreateContainer() *ContainerInfo {
 		panic(err)
 	}
 
-	return &ContainerInfo{
+	container := &ContainerInfo{
 		id:   strings.TrimSpace(string(stdout)),
 		port: port,
 	}
+
+	container.SaveNginxConf()
+
+	return container
+}
+
+func writeStringToFile(filename, content string) error {
+	// Open the file in write-only mode with file creation permission
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer file.Close()
+
+	// Write the string to the file
+	_, err = file.WriteString(content)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (container *ContainerInfo) SaveNginxConf() {
+	defaultConfig := fmt.Sprintf(`location /ttyd/%s {
+	proxy_http_version      1.1;
+	proxy_set_header        Host $host;
+	proxy_set_header        X-Real-IP $remote_addr;
+	proxy_set_header        X-Forwarded-Proto $scheme;
+	proxy_set_header        X-Forwarded-Proto $scheme;
+	proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+	proxy_set_header        Upgrade $http_upgrade;
+	proxy_set_header        Connection "upgrade";
+	proxy_pass              http://localhost:%s/;
+}`, container.id, strconv.Itoa(container.port))
+	writeStringToFile(container.NginxConfPath(), defaultConfig)
 }
 
 type ContainerPool struct {
 	items []ContainerInfo
 	size  int
+}
+
+func (container *ContainerInfo) NginxConfPath() string {
+	return "/etc/nginx/sites-available/location_" + container.id + ".conf"
+}
+
+func ReloadNginx() {
+	err := exec.Command("sudo", "systemctl", "reload", "nginx").Run()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func NewContainerPool(size int) *ContainerPool {
@@ -51,12 +100,15 @@ func NewContainerPool(size int) *ContainerPool {
 		fmt.Println(pool.items[i].id)
 	}
 
+	ReloadNginx()
+
 	return pool
 }
 
 func (pool *ContainerPool) DisposeContainerPool() {
 	args := []string{"stop"}
 	for _, item := range pool.items {
+		os.Remove(item.NginxConfPath())
 		args = append(args, item.id)
 	}
 
