d749b31f605024e1ff9053ce167c6229647d7439
Sravanth C <54331348+chebro@users.noreply.github.com>
2023-07-23 16:03:35 +0530

    added function to generate nginx config for weblink

--- nginx.go
+++ nginx.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
 )
@@ -17,6 +20,10 @@ func (container *ContainerInfo) NginxConfPath() string {
 	return "/etc/nginx/sites-available/location_" + container.id + ".conf"
 }
 
+func (container *ContainerInfo) WebLinkConfPath() string {
+	return "/etc/nginx/sites-available/" + container.id + ".hostahack.xyz"
+}
+
 func (container *ContainerInfo) SaveNginxConf() {
 	defaultConfig := fmt.Sprintf(`location /ttyd/%s {
 	proxy_http_version      1.1;
@@ -30,4 +37,28 @@ func (container *ContainerInfo) SaveNginxConf() {
 	proxy_pass              http://localhost:%s/;
 }`, container.id, strconv.Itoa(container.port))
 	WriteStringToFile(container.NginxConfPath(), defaultConfig)
+}
+
+func (container *ContainerInfo) GenerateWebLinkConfig() error {
+	if _, err := os.Stat(container.WebLinkConfPath()); err == nil {
+		// if file exists, delete the file
+		err := os.Remove(container.WebLinkConfPath())
+		if err != nil {
+			return err
+		}
+	} else if len(container.open_ports) == 0 {
+		// if file doesn't exist and there are no open ports, do nothing
+		return nil
+	} else {
+		// create a new config file
+		os.Create(container.WebLinkConfPath())
+		// for each port append the config to a file
+		for _, port := range container.open_ports {
+			link := sha256.Sum256([]byte(container.id + strconv.Itoa(port)))
+			short := hex.EncodeToString(link[:5])
+			SaveWebLinkConfig(short, container.ip, strconv.Itoa(port), container.WebLinkConfPath())
+		}
+		return nil
+	}
+	return nil
 }
--- utils.go
+++ utils.go
@@ -32,14 +32,74 @@ func CopyFolderToContainer(c *fiber.Ctx, savePath string) error {
 	if err != nil {
 		return err
 	}
+	// copy zip file to container
 	container_id := store.Get("container_id").(string)
 	containerPath := fmt.Sprintf("%s:/root", container_id)
 	err = exec.Command("docker", "cp", savePath, containerPath).Run()
 	if err != nil {
 		return err
 	}
+	// unzip the file inside the container
 	filePath := fmt.Sprintf("/root/%s.zip", store.ID())
 	err = exec.Command("docker", "exec", container_id, "unzip", filePath).Run()
+	if err != nil {
+		return err
+	}
+	// delete the zip file from container
 	err = exec.Command("docker", "exec", container_id, "rm", filePath).Run()
+	if err != nil {
+		return err
+	}
+	// delete zip file from host
+	err = os.Remove(savePath)
+	if err != nil {
+		return err
+	}
 	return nil
+}
+
+func SaveWebLinkConfig(link string, ip string, port string, filename string) {
+	webLink := fmt.Sprintf("%s.hostahack.xyz", link)
+	config := fmt.Sprintf(`
+server {
+	listen          80;
+	server_name     hostahack.xyz;
+	return 301      https://%s$request_uri;
+}
+server {
+        listen 443;
+        server_name                     %s;
+        ssl_certificate                 /etc/letsencrypt/live/hostahack.xyz/fullchain.pem;
+        ssl_certificate_key             /etc/letsencrypt/live/hostahack.xyz/privkey.pem;
+        ssl                             on;
+        
+        ssl_session_cache               builtin:1000    shared:SSL:10m;
+        ssl_protocols                   TLSv1.2         TLSv1.3;
+        ssl_ciphers                     HIGH:!aNULL:!eNULL:!EXPORT:!CAMELLIA:!DES:!MD5:!PSK:!RC4;
+        ssl_prefer_server_ciphers       on;
+
+        location / {
+                proxy_http_version      1.1;
+                proxy_set_header        Host $host;
+                proxy_set_header        X-Real-IP $remote_addr;
+                proxy_set_header        X-Forwarded-Proto $scheme;	
+                proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header        Upgrade $http_upgrade;
+                proxy_set_header        Connection "upgrade";
+                proxy_pass              %s:%s;
+                proxy_read_timeout      90;
+	}
+}
+    `, webLink, webLink, ip, port)
+
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	if _, err = f.WriteString(config); err != nil {
+		panic(err)
+	}
 }
